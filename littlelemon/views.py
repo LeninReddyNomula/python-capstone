37e991389bf747509820e94b4f183d2a731fa0b8
LeninReddyNomula <90997379+LeninReddyNomula@users.noreply.github.com>
2024-04-22 12:55:32 -0400

    Update views.py

--- littlelemon/views.py
+++ littlelemon/views.py
@@ -1,124 +1,48 @@
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status,generics
-from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin,CreateModelMixin 
-from rest_framework.response import Response
-from .serializers import UserSerializer, PasswordSerializer,MenuSerializer,BookingSerializer
+# from django.http import HttpResponse
+from django.shortcuts import render
 
-#models
-from .models import Menu,BookingTable
+from .models import Menu
+from django.core import serializers
+from .models import Booking
+from datetime import datetime
+import json
+from .forms import BookingForm
 
-class UserView(viewsets.ModelViewSet):
-    
-    
-    
-    def get_queryset(self):
-        return User.objects.all()
+def home(request):
+    return render(request, 'index.html')
 
-    def get_serializer_class(self):
-        if self.action == 'set_password':
-            return PasswordSerializer
-        else:
-            return UserSerializer
-        
-    @action(detail=True, methods=['post','get'])
-    def set_password(self,request,pk=None):
-        user  =User.objects.get(pk=pk)
-        print(request.method)
-        if request.method == 'POST':
-            new_password = request.POST.get('password')
-            serializer_class = self.get_serializer_class()
-            serializer =serializer_class(data = {'password':new_password},context={'request':request})
-            
-            if serializer.is_valid():
-                user.set_password(serializer.validated_data['password'])
-                user.save()
-                print("am here")
-                return Response({'status': 'Password set successfully'}, status=status.HTTP_200_OK)
-            else:
-                return Response({"something wrong":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-        
-            return Response("")#serialier.data, status=status.HTTP_200_OK)
+def about(request):
+    return render(request, 'about.html')
 
-        
-    def list(self,request):
-        serializer_class = self.get_serializer_class()
-        queryset = self.get_queryset()
-        
-        serializer = serializer_class(queryset,many=True,context={'request':request})
-        return Response(serializer.data, status=status.HTTP_200_OK)
-            
-    def create(self, request):
-        queryset = self.get_queryset()  # Accessing get_queryset method
-        serializer_class = self.get_serializer_class()
-        serializer = serializer_class(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND) 
-    
-    
-class MenuItemView(generics.ListCreateAPIView,CreateModelMixin,viewsets.GenericViewSet):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
-    
-    def list(self, request):
-        
-        serializer = self.serializer_class(self.get_queryset(),many=True,context={'request':request})
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-    
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+# Add code for the bookings() view
+def bookings(request):
+    data = request.GET.get('date', datetime.today().date())
+
+    bookings = Booking.objects.all()
+    booking_json = serializers.serialize('json', bookings)
+
+    return render(request, "bookings.html", {"bookings": booking_json, "bookings_form": bookings})
 
 
-class SingleMenuItem(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
-    
-    def retrieve(self, request, pk=None):
-        menuitem = Menu.objects.get(pk=pk)
-        print(menuitem)
-        serializer = self.serializer_class(menuitem)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-        
-    def destroy(self, request, pk=None):
-        menuitem = Menu.objects.get(pk=pk)
-        
-        if menuitem:
-            menuitem.delete()
-            return Response({'message:Deleted successfully'},status=status.HTTP_200_OK)
-        else:
-            return Response({'message:Item not available'},status=status.HTTP_400_BAD_REQUEST)
-        
+
+def menu(request):
+    menu_data = Menu.objects.all()
+    main_data = {"menu": menu_data}
+    return render(request, 'menu.html', {"menu": main_data})
 
 
-class BookingViewSet(viewsets.ModelViewSet):
-    queryset = BookingTable.objects.all()
-    serializer_class = BookingSerializer
-    
-    
-    def list(self,request):
-        
-        data = self.get_queryset()
-        serializer = self.serializer_class(data,context={'request':request},many=True)
-        
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-    def destroy(self, request, pk=None):
-        bookings = self.get_queryset()
-        booking = get_object_or_404(bookings,pk=pk)
-        booking.delete()  # Delete the booking instance
-        return Response({"message:deleted successfully"},status=status.HTTP_204_NO_CONTENT)
-        
-    
-    def retrieve(self,request,pk=None):
-        queryset = self.get_queryset()
-        booking = get_object_or_404(queryset,pk=pk)
-        
-        serializer = self.serializer_class(booking,context = {'request':request})
-        return Response(serializer.data,status=status.HTTP_200_OK)
-        
-        
-        
+def display_menu_item(request, pk=None): 
+    if pk: 
+        menu_item = Menu.objects.get(pk=pk) 
+    else: 
+        menu_item = "" 
+    return render(request, 'menu_item.html', {"menu_item": menu_item}) 
