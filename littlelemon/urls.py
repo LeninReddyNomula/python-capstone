10ac702b48c3b32e2e0c32176a54384954659ead
LeninReddyNomula <90997379+LeninReddyNomula@users.noreply.github.com>
2024-04-22 12:55:59 -0400

    Update urls.py

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -1,25 +1,12 @@
-from django.urls import path,include
-from rest_framework.routers import DefaultRouter,SimpleRouter
+from django.urls import path
 from . import views
-
-# router = SimpleRouter()
-# router.register(r'users', views.UserView,basename='user')
-
-
-router = DefaultRouter()
-router.register(r'users', views.UserView,basename='user')
-router.register(r'booking',views.BookingViewSet,basename='booking')
-
 
 
 urlpatterns = [
-
-   # path('user/',views.UserView.as_view({'get':'list','post':'create'}) ),
-    path('',include(router.urls)),
-    path('items/',views.MenuItemView.as_view({'get':'list'})),
-    path('items/<int:pk>', views.SingleMenuItem.as_view()),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    #path('users/<int:pk>/set_password/', views.UserViewSet.as_view({'post': 'set_password'}), name='set_password'),
-    
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
+    path('bookings/', views.bookings, name="bookings"),
 ]
-
